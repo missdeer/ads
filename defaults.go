2e4a7d02713c4d6b186768bc12e844c6ca5bbeba
Fan Yang <me@minidump.info>
2022-09-22 10:20:43 +0800

    update black lists

--- defaults.go
+++ defaults.go
@@ -23,16 +23,14 @@ import (
 
 var defaultBlacklists = []string{
 	"https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
-	"https://mirror1.malwaredomains.com/files/justdomains",
 	"http://sysctl.org/cameleon/hosts",
-	"https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist",
 	"https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt",
 	"https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt",
+	"https://anti-ad.net/domains.txt",
 }
 
 var strictDefaultBlacklists = []string{
 	"https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
-	"https://mirror1.malwaredomains.com/files/justdomains",
 	"http://sysctl.org/cameleon/hosts",
 	"https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt",
 	"https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt",
@@ -40,11 +38,6 @@ var strictDefaultBlacklists = []string{
 	"https://raw.githubusercontent.com/StevenBlack/hosts/master/data/KADhosts/hosts",
 	"https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Spam/hosts",
 	"https://v.firebog.net/hosts/static/w3kbl.txt",
-	"https://v.firebog.net/hosts/BillStearns.txt",
-	"https://www.dshield.org/feeds/suspiciousdomains_Low.txt",
-	"https://www.dshield.org/feeds/suspiciousdomains_Medium.txt",
-	"https://www.dshield.org/feeds/suspiciousdomains_High.txt",
-	"https://www.joewein.net/dl/bl/dom-bl-base.txt",
 	"https://raw.githubusercontent.com/matomo-org/referrer-spam-blacklist/master/spammers.txt",
 	"https://hostsfile.org/Downloads/hosts.txt",
 	"https://someonewhocares.org/hosts/zero/hosts",
@@ -58,21 +51,18 @@ var strictDefaultBlacklists = []string{
 	"https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt",
 	"https://v.firebog.net/hosts/Easylist.txt",
 	"https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts;showintro=0",
-	"https://raw.githubusercontent.com/StevenBlack/hosts/master/data/UncheckyAds/hosts",
-	"https://www.squidblacklist.org/downloads/dg-ads.acl",
 	"https://v.firebog.net/hosts/Easyprivacy.txt",
 	"https://v.firebog.net/hosts/Prigent-Ads.txt",
 	"https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt",
 	"https://raw.githubusercontent.com/StevenBlack/hosts/master/data/add.2o7Net/hosts",
 	"https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
 	"https://zerodot1.gitlab.io/CoinBlockerLists/hosts",
-	"http://www.malwaredomainlist.com/hostslist/hosts.txt",
 	"http://www.malwaredomainlist.com/hostslist/delisted.txt",
-	"https://raw.github.com/jonschipp/mal-dnssearch/master/mandiant_apt1.dns",
+	"https://raw.githubusercontent.com/jonschipp/mal-dnssearch/master/mandiant_apt1.dns",
 }
 
 var strictDefaultWhitelists = []string{
-	"https://raw.github.com/missdeer/ads/master/lists/strict-whitelist.txt",
+	"https://raw.githubusercontent.com/missdeer/ads/master/lists/strict-whitelist.txt",
 }
 
 const defaultIPv4ResolutionIP = "127.0.0.1"
